a1888be369a9db974a0d4fa1af0778d4a9825fab
Sergey <94394415+des-808@users.noreply.github.com>
2023-10-18 01:40:22 +0300

    vvvv

--- DesourceDB.sql
+++ DesourceDB.sql
@@ -1,17 +1,20 @@
-﻿create database ResourceDB;
-go
-use ResourceDB;
-go
-
-create table Resources
-(
-Id int identity(1,1) primary key,
-Name nvarchar(255) not null
-
-)
+﻿--create database HeroesDB;
+--go
+use HeroesDB;
 go
 
-insert into Resources(Name) values('База данных');
-insert into Resources(Name) values('Общая папка на сервере');
-insert into Resources(Name) values('Excel-таблица с товарами');
+--create table Heroes
+--(
+--Id int identity(1,1) primary key,
+--Name nvarchar(255) not null,
+--Race nvarchar(255) not null,
+--Age int,
+--Weapon nvarchar(255) not null
+
+--)
+--go
+
+insert into Heroes values(,'База данных','xz',100,'cv');
+insert into Heroes(Name) values(,'Общая папка на сервере','asd',20,'zxc');
+insert into Heroes(Name) values(,'Excel-таблица с товарами','vpn',80,'vnn');
 go
--- Hero.cs
+++ Hero.cs
@@ -49,11 +49,3 @@ namespace WpfAppEntity
         }
     }
 }
-//internal class Hero
-//{
-//    public int Id { get; set; }
-//    public string? Name { get; set; }
-//    public string? Race { get; set; }
-//    public int Age { get; set; }
-//    public string? Weapon { get; set; }
-//}
